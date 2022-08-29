27cdf6351702c57f7620f3ef7415388365ce4a42
k8ie <k8ie@mcld.eu>
2022-08-29 15:04:43 +0100

    Remove the container after building

--- build-site.sh
+++ build-site.sh
@@ -5,4 +5,5 @@ SITE_DST="/srv/site"
 docker create --cidfile "/tmp/jekyllcid" --volume="$SITE_DST:/dst" jekyll/jekyll bash -c 'chown 1000:1000 -R /src && jekyll build --source /src --destination /dst'
 docker cp "$SITE_SRC" "$(cat /tmp/jekyllcid):/src/"
 docker start -i -a "$(cat /tmp/jekyllcid)"
+docker container rm "$(cat /tmp/jekyllcid)"
 rm /tmp/jekyllcid
