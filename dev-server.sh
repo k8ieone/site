8dbed853fe93a2e72ffa4893fc788191237401f3
k8ie <k8ie@mcld.eu>
2024-08-19 22:37:58 +0200

    Fix incorrect timestamps

--- dev-server.sh
+++ dev-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SITE_SRC="$PWD"
 
-docker run --user $UID:$GID -it --rm --mount type=bind,src="$SITE_SRC",target=/src -p 1313:1313 ghcr.io/hugomods/hugo:latest sh -c 'cd /src && hugo server --bind 0.0.0.0 --disableFastRender'
+docker run --user $UID:$GID -it --rm --mount type=bind,src=/etc/localtime,target=/etc/localtime,readonly --mount type=bind,src="$SITE_SRC",target=/src -p 1313:1313 ghcr.io/hugomods/hugo:latest sh -c 'cd /src && hugo server --bind 0.0.0.0 --disableFastRender'
--- new-post.sh
+++ new-post.sh
@@ -2,4 +2,4 @@
 SITE_SRC="$PWD"
 FILENAME="$1"
 
-docker run --user $UID:$GID -it --rm --mount type=bind,src="$SITE_SRC",target=/src ghcr.io/hugomods/hugo:latest sh -c "cd /src && hugo new content blog/posts/$FILENAME/index.md"
+docker run --user $UID:$GID -it --rm --mount type=bind,src=/etc/localtime,target=/etc/localtime,readonly --mount type=bind,src="$SITE_SRC",target=/src ghcr.io/hugomods/hugo:latest sh -c "cd /src && hugo new content blog/posts/$FILENAME/index.md"
