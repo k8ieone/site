a0300112983da0e81faf42328c6edf97ae117fb4
k8ie <k8ie@mcld.eu>
2025-10-19 23:28:59 +0200

    Small tweak

--- new-post.sh
+++ new-post.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 SITE_SRC="$PWD"
 FILENAME="$1"
+DATEPREFIX=$(date +%Y-%m-%d)
 
-docker run --user $UID:$GID -it --rm --mount type=bind,src=/etc/localtime,target=/etc/localtime,readonly --mount type=bind,src="$SITE_SRC",target=/src ghcr.io/hugomods/hugo:latest sh -c "cd /src && hugo new content blog/posts/$FILENAME/index.md"
+#mkdir -p content/blog/posts/"$DATEPREFIX-$FILENAME"
+docker run --user $UID:$GID -it --rm --mount type=bind,src=/etc/localtime,target=/etc/localtime,readonly --mount type=bind,src="$SITE_SRC",target=/src ghcr.io/hugomods/hugo:latest sh -c "cd /src && hugo new content blog/posts/$DATEPREFIX-$FILENAME/index.md"
+xdg-open content/blog/posts/"$DATEPREFIX-$FILENAME"/index.md
