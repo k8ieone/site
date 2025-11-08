2ab572ed672684f1aa476560e4627ce4980a3b9c
k8ie <k8ie@mcld.eu>
2025-11-08 20:09:45 +0100

    Quit on error and enable exec logging

--- dev-server.sh
+++ dev-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -xe
 SITE_SRC="$PWD"
 
 docker run --user $UID:$GID -it --rm --mount type=bind,src=/etc/localtime,target=/etc/localtime,readonly --mount type=bind,src="$SITE_SRC",target=/src -p 1313:1313 ghcr.io/hugomods/hugo:dart-sass sh -c 'cd /src && hugo server --bind 0.0.0.0 --disableFastRender --buildDrafts'
